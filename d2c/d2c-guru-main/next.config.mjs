37b23619c7d57537124ef467c447bfe40b721a32
Yash Solanki <159527978+yash-k-s-7span@users.noreply.github.com>
2024-12-11 13:33:21 +0530

    Delete d2c directory

--- d2c/d2c-guru-main/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-};
-
-export default nextConfig;
